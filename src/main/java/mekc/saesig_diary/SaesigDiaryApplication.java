381fbe5749eed97bf230af36d54cb36023e9c648
wonjjong <wonjjong.dev@gmail.com>
2023-02-26 22:55:29 +0900

    스프링 부트 프로젝트 생성

---
+++ src/main/java/mekc/saesig_diary/SaesigDiaryApplication.java
@@ -0,0 +1,12 @@
+package mekc.saesig_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaesigDiaryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SaesigDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mekc/saesig_diary/SaesigDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package mekc.saesig_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaesigDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
