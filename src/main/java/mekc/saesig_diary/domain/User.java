7c10184188c7fb0d3ea85071cb5c55fcf2a7da64
wonjjong <wonjjong.dev@gmail.com>
2023-03-01 22:44:14 +0900

    feat(jpa): User 관련 엔티티 클래스 추가

---
+++ src/main/java/mekc/saesig_diary/domain/User.java
@@ -0,0 +1,45 @@
+package mekc.saesig_diary.domain;
+
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String status;
+
+    @Column
+    private String name;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, Role role, String password) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.status = status;
+    }
+
+}
