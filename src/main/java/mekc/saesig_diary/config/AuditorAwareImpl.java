3aa35dcbee228f30c488f5332c1d091eebc1b04c
wonjjong <wonjjong.dev@gmail.com>
2023-03-01 22:43:58 +0900

    feat(jpa): JPA 관련 config / auditor 클래스 추가

---
+++ src/main/java/mekc/saesig_diary/config/AuditorAwareImpl.java
@@ -0,0 +1,14 @@
+package mekc.saesig_diary.config;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+
+public class AuditorAwareImpl implements AuditorAware<Long> {
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        // TODO : OAUTH / JWT 방식 선택 후 로직 구현 해야함
+        return Optional.empty();
+    }
+}
