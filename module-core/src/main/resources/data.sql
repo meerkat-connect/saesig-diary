29ce923d8303b9ec6783a8c6ca81a0382811baba
wonjjong <wonjjong.dev@gmail.com>
2023-04-24 00:30:43 +0900

    chore : module-core data.sql 수정

--- module-core/src/main/resources/data.sql
+++ module-core/src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO member(id, email, password) VALUES (1, 'wonjjong.dev@gmail.com', '1qaz2wsx!@')
+INSERT INTO member(id, email, password, signup_method, status, joined_at, modified_at, modified_by, created_at, created_by)
+VALUES (1, 'wonjjong.dev@gmail.com', '1qaz2wsx!@', 'EMAIL', 'NORMAL', NOW(), NOW(), 1, NOW(), 1);
