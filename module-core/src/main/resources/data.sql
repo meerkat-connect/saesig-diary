451b334cc201c563e9223f6f49a199396963609e
wonjjong <wonjjong.dev@gmail.com>
2023-05-01 14:41:50 +0900

    chore: member 테이블 동의 여부 컬럼들 추가

--- module-core/src/main/resources/data.sql
+++ module-core/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO member(id, email, password, signup_method, status, joined_at, modified_at, modified_by, created_at, created_by)
-VALUES (1, 'wonjjong.dev@gmail.com', '1qaz2wsx!@', 'EMAIL', 'NORMAL', NOW(), NOW(), 1, NOW(), 1);
+INSERT INTO member(id, email, password, signup_method, status, joined_at, modified_at, modified_by, created_at, created_by, serivce_agreement, privacy_agreement)
+VALUES (1, 'wonjjong.dev@gmail.com', '1qaz2wsx!@', 'EMAIL', 'NORMAL', NOW(), NOW(), 1, NOW(), 1, 'Y', 'Y');
--- module-core/src/main/resources/schema.sql
+++ module-core/src/main/resources/schema.sql
@@ -10,7 +10,11 @@ CREATE TABLE `member`
     `modified_at`   DATETIME     NOT NULL COMMENT '수정일',
     `modified_by`   BIGINT       NOT NULL COMMENT '수정자 일련번호',
     `created_at`    DATETIME     NOT NULL COMMENT '등록일',
-    `created_by`    BIGINT       NOT NULL COMMENT '등록자 일련번호'
+    `created_by`    BIGINT       NOT NULL COMMENT '등록자 일련번호',
+    `service_agreement`	CHAR(1)	NOT NULL	COMMENT '서비스 이용약관 동의 여부',
+    `privacy_agreement`	CHAR(1)	NOT NULL	COMMENT '개인정보 수집 및 이용동의',
+    `location_service_agreement`	CHAR(1)	NOT NULL	DEFAULT 'N'	COMMENT '위치 기반 서비스 이용동의',
+    `marketing_service_agreement`	CHAR(1)	NOT NULL	DEFAULT 'N'	COMMENT '마케팅 서비스 이용동의'
 );
 
 ALTER TABLE `member`
