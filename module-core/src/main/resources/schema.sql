62df8ba62296abbbece882b4b365c10efd33e48f
wonjjong <wonjjong.dev@gmail.com>
2023-05-01 17:06:00 +0900

    chore: schema.sql 파일 / 파일그룹 테이블 추가

--- module-core/src/main/resources/schema.sql
+++ module-core/src/main/resources/schema.sql
@@ -20,8 +20,7 @@ CREATE TABLE `member`
 ALTER TABLE `member`
     ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (`id`);
 
-CREATE TABLE `file`
-(
+CREATE TABLE `file` (
     `id`          BIGINT       NOT NULL COMMENT '파일 일련번호',
     `group_id`    BIGINT       NOT NULL COMMENT '파일 그룹 일련번호',
     `path`        VARCHAR(500) NOT NULL COMMENT '경로',
@@ -29,25 +28,23 @@ CREATE TABLE `file`
     `origin_name` VARCHAR(500) NOT NULL COMMENT '원본 파일 이름',
     `extension`   VARCHAR(20)  NOT NULL COMMENT '확장자',
     `size`        INTEGER      NOT NULL COMMENT '크기',
-    `ord`         SMALLINT     NULL COMMENT '순서',
+    `ord`         SMALLINT NULL COMMENT '순서',
     `modified_at` DATETIME     NOT NULL COMMENT '수정일',
-    `modified_by` BIGINT       NULL COMMENT '수정자 아이디',
+    `modified_by` BIGINT       NOT NULL COMMENT '수정자 아이디',
     `created_at`  DATETIME     NOT NULL COMMENT '등록일',
-    `created_by`  BIGINT       NULL COMMENT '등록자 아이디'
+    `created_by`  BIGINT       NOT NULL COMMENT '등록자 아이디'
 );
 
 ALTER TABLE `file`
     ADD CONSTRAINT `PK_FILE` PRIMARY KEY (`id`);
 
-CREATE TABLE `file_group`
-(
-    `id`             BIGINT       NOT NULL COMMENT '파일 그룹 일련번호',
+CREATE TABLE `file_group` (
+    `id`             BIGINT   NOT NULL COMMENT '파일 그룹 일련번호',
     `directory_path` VARCHAR(200) NULL COMMENT '디렉토리 경로명',
-    `board_id`       INTEGER      NULL COMMENT '게시판 아이디',
-    `modified_at`    DATETIME     NOT NULL COMMENT '수정일',
-    `modified_by`    BIGINT       NOT NULL COMMENT '수정자 아이디',
-    `created_at`     DATETIME     NOT NULL COMMENT '등록일',
-    `created_by`     BIGINT       NOT NULL COMMENT '등록자 아이디'
+    `modified_at`    DATETIME NOT NULL COMMENT '수정일',
+    `modified_by`    BIGINT   NOT NULL COMMENT '수정자 아이디',
+    `created_at`     DATETIME NOT NULL COMMENT '등록일',
+    `created_by`     BIGINT   NOT NULL COMMENT '등록자 아이디'
 );
 
 ALTER TABLE `file_group`
