9f65bd9ade96a9a7eabf29a94eba1a6bec9cf39c
wonjjong <wonjjong.dev@gmail.com>
2023-05-08 23:14:27 +0900

    feat: 응답에 사용될 FileDto 파일 필드 추가

--- module-api/src/main/java/saesigDiary/File/FileDto.java
+++ module-api/src/main/java/saesigDiary/File/FileDto.java
@@ -1,4 +1,18 @@
 package saesigDiary.File;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class FileDto {
+    private final String originName;
+    private final String savedName;
+    private final String path;
+
+    @Builder
+    public FileDto(String originName, String savedName, String path) {
+        this.originName = originName;
+        this.savedName = savedName;
+        this.path = path;
+    }
 }
