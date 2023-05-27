9245742b88f3ba3094e261c7ab2665bdac483173
wonjjong <wonjjong.dev@gmail.com>
2023-05-27 16:23:59 +0900

    feat: p6Spy 설정 추가

---
+++ module-core/src/main/java/saesigDiary/config/P6SpyConfig.java
@@ -0,0 +1,8 @@
+package saesigDiary.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class P6SpyConfig {
+
+}
---
+++ module-core/src/test/java/saesigDiary/config/CustomDataJpaTest.java
@@ -0,0 +1,21 @@
+package saesigDiary.config;
+
+
+import com.github.gavlyukovskiy.boot.jdbc.decorator.DataSourceDecoratorAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@DataJpaTest(showSql = false)
+@ImportAutoConfiguration(DataSourceDecoratorAutoConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(P6SpyConfig.class)
+public @interface CustomDataJpaTest {
+}
