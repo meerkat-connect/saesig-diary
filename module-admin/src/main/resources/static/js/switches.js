0ae468c03e29a0335ac6de8d3e338912516b6bf5
wonjjong <wonjjong.dev@gmail.com>
2023-06-10 10:22:18 +0900

    feat: 관리자 공통 js 파일 추가

--- module-admin/src/main/resources/static/js/switches.js
+++ module-admin/src/main/resources/static/js/switches.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     // Single swithces small (22/11/16 JM sm버전을 위해 추가함
     var elemsingleSmallList = document.querySelectorAll('.js-single-small');
     for(var i=0; i < elemsingleSmallList.length; i++){
-        switchCherryArr[elemsingleSmallList[i].getAttribute('id')]  = new Switchery(elemsingleSmallList[i], { color: '#93BE52', jackColor: '#fff', size : 'small'});
+        switchCherryArr[elemsingleSmallList[i].getAttribute('id')]  = new Switchery(elemsingleSmallList[i], { color: '#FF8031', jackColor: '#fff', size : 'small'});
     }
     if(switchCherryCallback != undefined && typeof switchCherryCallback === 'function'){
         switchCherryCallback();
