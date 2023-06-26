1930f833ee22e26bc6a96a270fc55e00013eb2a1
wonjjong <wonjjong.dev@gmail.com>
2023-06-27 00:36:17 +0900

    design : custom select 태그 구현

--- module-admin/src/main/resources/static/js/custom-design.js
+++ module-admin/src/main/resources/static/js/custom-design.js
@@ -1,5 +1,8 @@
 (function ($) {
-    $(document).on('click', '.file-open-btn', function() {
+
+    $(document).on('click', '.file-open-btn', function () {
         $(this).parent().find('input').click();
-    })
+    });
+
+    $('.custom-select').niceSelect();
 }(jQuery));
