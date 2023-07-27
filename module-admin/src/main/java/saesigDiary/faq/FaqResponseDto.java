d8b99557c713b7caa3bc82bac9a2517733b10c5f
wonjjong <wonjjong.dev@gmail.com>
2023-07-27 17:57:54 +0900

    refactor: Faq 응답 DTO 등록일 타입 수정

--- module-admin/src/main/java/saesigDiary/faq/FaqResponseDto.java
+++ module-admin/src/main/java/saesigDiary/faq/FaqResponseDto.java
@@ -1,30 +1,39 @@
 package saesigDiary.faq;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 import saesigDiary.domain.faq.Faq;
 import saesigDiary.domain.faq.FaqCategory;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @ToString
+@NoArgsConstructor
 public class FaqResponseDto {
     private Long id;
     private String title;
+    private String content;
     private FaqCategory category;
     private Long ord;
     private Character isEnabled;
     private String createdBy;
-    private LocalDate createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdAt;
 
     public FaqResponseDto(Faq faq) {
         this.id = faq.getId();
         this.title = faq.getTitle();
+        this.content = faq.getContent();
         this.category = faq.getCategory();
         this.ord = faq.getOrd();
         this.isEnabled = faq.getIsEnabled();
-        this.createdAt = faq.getCreatedAt().toLocalDate();
+        this.createdAt = faq.getCreatedAt();
         this.createdBy = faq.getCreatedBy().getNickname();
     }
 }
