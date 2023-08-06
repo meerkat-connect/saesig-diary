d2d3b43cc1b4acbb8ec4a73a78eca18b8bf86cc8
wonjjong <wonjjong.dev@gmail.com>
2023-08-06 15:36:31 +0900

    feat: 로그인 실패시 실패 메시지 추가

--- module-admin/src/main/java/com/saesig/common/CommonController.java
+++ module-admin/src/main/java/com/saesig/common/CommonController.java
@@ -1,12 +1,20 @@
 package com.saesig.common;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CommonController {
     @GetMapping("/admin/login")
-    public String login() {
+    public String login(@RequestParam(required = false) Boolean error,
+                        @RequestParam(required = false) String errorMessage,
+                        Model model) {
+
+        model.addAttribute("error", error);
+        model.addAttribute("errorMessage", errorMessage);
+
         return "common/login";
     }
 }
