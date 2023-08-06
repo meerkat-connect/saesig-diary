5b483746becaa5e16477cb5154018c12cdccc31f
wonjjong <wonjjong.dev@gmail.com>
2023-08-06 17:37:47 +0900

    fix: @Param import 패키지 변경

--- module-core/src/main/java/com/saesig/domain/member/MemberRepository.java
+++ module-core/src/main/java/com/saesig/domain/member/MemberRepository.java
@@ -1,14 +1,14 @@
 package com.saesig.domain.member;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Page<Member> findAll(Pageable pageable);
 
     @Query("SELECT m FROM member m WHERE m.email=:email ")
