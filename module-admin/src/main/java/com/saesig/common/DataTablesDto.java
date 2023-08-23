bdc20d59aad2f2e6620c6dee97ae667fcb38ba77
서정도 <sjd4328@kbrainc.com>
2023-08-23 15:15:53 +0900

    ongoing : dataTables paging 추가

---
+++ module-admin/src/main/java/com/saesig/common/DataTablesDto.java
@@ -0,0 +1,29 @@
+package com.saesig.common;
+
+import lombok.Data;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class DataTablesDto {
+
+    // draw            : DataTables의 Data 순서 보장하기 위한 객체이다. 1부터 시작하며 해당 tables 하단의 페이지번호와는 무관하다.
+    // recordsTotal    : 필터링 전 총 Total Count이다.
+    // recordsFiltered : 필터링 후 총  Total Count이다. DataTalbles의 필터기능을 이용하여 paging을 할 경우 사용되는것 같다. 해당 프로젝트에선 사용하지 않고있다.
+    // data            : DataTables를 paging 하기 위해선 해당 Dto를 return 해줘야하는데 목록 data를 여기에 담아서 반환해주면 된다.
+
+    private int draw;
+    private int recordsTotal;
+    private int recordsFiltered;
+
+    private List data;
+
+    public List getData(){
+        if(CollectionUtils.isEmpty(data)){
+            data = new ArrayList();
+        }
+        return data;
+    }
+}
--- module-admin/src/main/java/com/saesig/common/RequestDto.java
+++ module-admin/src/main/java/com/saesig/common/RequestDto.java
@@ -1,8 +1,9 @@
 package com.saesig.common;
 
-import lombok.Getter;
+import com.saesig.util.StringUtil;
+import lombok.Data;
 
-@Getter
+@Data
 public class RequestDto {
     private String searchType;
     private String searchName;
@@ -14,5 +15,57 @@ public class RequestDto {
         this.start = start;
         this.length = length;
         this.pageNum = pageNum;
+    }
+
+    protected Integer draw;
+    protected Integer recordsTotal;
+    protected Integer rowNumber;
+
+    public static final ORDER_DIRECTION ORDER_DIRECTION_ASC = ORDER_DIRECTION.asc;
+    public static final ORDER_DIRECTION ORDER_DIRECTION_DESC = ORDER_DIRECTION.desc;
+
+    public int pageNumber;
+    public int rowPerPage;
+    public int pageOffset;
+    public String orderField;
+    public ORDER_DIRECTION orderDirection;
+
+    public enum ORDER_DIRECTION {
+        asc, desc
+    }
+
+    public RequestDto() {
+        this.chkParams();
+    }
+
+    public Integer getPageNumber(Integer start, Integer length) {
+        this.pageNumber = (start/length);
+        if(this.pageNumber == 0) {
+            this.pageNumber = 1;
+        }
+        return this.pageNumber;
+    }
+
+    public void setOrderField(String orderField) {
+        if (StringUtil.isSqlInjectionSafe(orderField)) {
+            this.orderField = orderField;
+        }
+    }
+    public void chkParams() {
+        if (this.pageNumber == 0) {
+            this.pageNumber = 1;
+        }
+
+        if (this.rowPerPage == 0) {
+            this.rowPerPage = 10;
+        }
+
+        if (this.orderField == null || "".equals(this.orderField)) {
+            this.orderField = "MODIFIED_AT";
+        }
+
+        if (this.orderDirection == null) {
+            this.orderDirection = ORDER_DIRECTION.desc;
+        }
     }
 }
