4d9eecf9adcff399f82eebd8ccfe6b7abb65dc26
wonjjong <wonjjong.dev@gmail.com>
2023-08-27 18:47:50 +0900

    chore: s3 관련 라이브러리 및 설정 추가

--- module-admin/src/main/java/com/saesig/SaesigDiaryApplication.java
+++ module-admin/src/main/java/com/saesig/SaesigDiaryApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SaesigDiaryApplication {
+
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SaesigDiaryApplication.class, args);
     }
