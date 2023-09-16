f6aef80a59209c918da257eb428a523f0bec6ef3
wonjjong <wonjjong.dev@gmail.com>
2023-09-16 16:01:51 +0900

    ongoing: jquery Validation 작업 중간 커밋

---
+++ module-admin/src/main/resources/static/js/jqueryValidateAdd.js
@@ -0,0 +1,10 @@
+// ckRequired
+// ckeditor 필수입력 유효성 체크
+/*$.validator.addMethod('ckRequired', function (value, element) {
+    if (value == null || value == 'undefined') return false;
+
+    var content = value.replaceAll("&nbsp;", "").trim();
+
+    if (content == '' || content.length == 0) return false;
+    else return true;
+});*/
---
+++ module-admin/src/main/resources/static/js/jqueryValidationSetting.js
@@ -0,0 +1,84 @@
+/**
+ * 밸리데이터 디폴 값 변경
+ * @since 19.04.11
+ *
+ * **/
+//밸리데이터 옵션  alert 형식으로 변경
+$.validator.setDefaults({
+    onkeyup:false, // 키를 뗄대 유효성 검사 off
+    onclick:false, // checkbox와 radio 버튼 클릭시 유효성 검사 off
+    onfocusout:false, // 포커스가 떠날때 유효성 검사 off
+    errorElement: "input",
+    highlight :true,
+    // focusInvalid: true, 유형 검사 후 포커서를 해당 무효 필드에 둘 것인가 여부
+    // focusCleanup: true true로 설정되어 있는 경우 잘못된 필드에 포커스가 가면 에러메시지를 지운다.
+    // errorClass: 'invalid',
+    // validClass: 'success',
+    showErrors:function(errorMap, errorList){ //alert
+        if(this.numberOfInvalids() && errorList.length != 0) {
+            //option1 : alert 표시
+            alert(errorList[0].message);
+
+            errorList[0].element.focus();
+            //option2  : 입력 값 지우고 에러메세지 placeholder 입력
+            //*$(errorList[0].element).val(null);
+            //$(errorList[0].element).attr("placeholder",errorList[0].message);
+
+            let $el = $(errorList[0].element)[0];
+
+            if($el.hasAttribute('selectsearchcondition')){
+                let borderEl =  $($el).siblings('.select2-container').find('.select2-selection');
+                $(borderEl).css("border","2px solid #ff0000").animate({}, 500);//#B0371D : 진한빨강 ,
+                setTimeout(function() { $(borderEl).css("border","")}, 1000);
+            }else{
+                //if($(errorList[0].element).parent("<div>") )
+                //option3 : input border 라인 red 강조 수정
+                $($el).css("border","2px solid #ff0000");//#B0371D : 진한빨강 ,
+                setTimeout(function() { $($el).css("border","")}, 1000);
+            }
+        }
+    }
+});
+
+//default message 값 변경 English -> korean
+$.extend( $.validator.messages,
+    {
+        //jquery.validate.js 디폴트 메소드 및 메세지
+        required: "필수 입력 항목입니다."
+        , remote: "항목을 수정해 주십시오."
+        , email: "유효하지 않은 E-Mail주소입니다."
+        , url: "유효하지 않은 URL입니다."
+        , date: "올바른 날짜를 입력하십시오."
+        , dateISO: "올바른 날짜(ISO)를 입력하십시오."
+        , number: "숫자만 입력가능합니다."
+        , digits: "숫자만 입력 가능합니다."
+        , creditcard: "신용카드 번호가 바르지 않습니다."
+        , equalTo: "같은 값을 다시 입력하십시오."
+        , extension: "올바른 확장자가 아닙니다."
+        , maxlength: $.validator.format( "{0}자를 넘을 수 없습니다. " ) //maxlength:[8]
+        , minlength: $.validator.format( "{0}자 이상 입력하십시오." )
+        , rangelength: $.validator.format( "문자 길이가 {0} 에서 {1} 사이의 값을 입력하십시오." ) //rangelength: [2, 10]
+        , range: $.validator.format( "{0} 에서 {1} 사이의 값을 입력하십시오." ) //range: [2, 10]
+        , max: $.validator.format( "{0} 이하의 값을 입력하십시오." )
+        , min: $.validator.format( "{0} 이상의 값을 입력하십시오." )
+        /***************** custom Method*******************
+         * jquey.validate.add.js  추가 메소드
+         *************************************************/
+        , alphanumeric: "알파벳과 숫자만 입력 가능합니다."
+        , passwordPolicy1 : "영문,숫자만 입력 가능합니다."
+        , passwordPolicy2 : "영문,숫자,특수문자 가 하나 이상 입력되어야 합니다."
+        , isBlank :"공백이 있습니다."
+        , isHangulOn : "한글만 입력가능합니다."
+        , isHangulOff : "한글은 입력 할 수 없습니다."
+        , isSybolOff : "특수기호는 사용 할 수 없습니다."
+        , isRightId : "올바르지 않은 주민번호입니다."
+        , isRightBizNo : "올바르지 않는 사업자 번호입니다."
+        , emailDomain : "올바른 이메일을 입력해주십시오."
+        , hangulOrAlpha : "한글과 알파벳만 사용가능합니다."
+        , ckRequired: '내용을 입력해 주십시오.'
+    }
+
+);
+
+
+
