ec066d3de1bd28dcb1daa7809af00833c3d254d5
wonjjong <wonjjong.dev@gmail.com>
2023-10-08 02:36:51 +0900

    feat: datatables 공통dto 페이지 번호 및 페이징 크기 변수 추가

--- module-admin/src/main/java/com/saesig/role/DataTablesResponseDto.java
+++ module-admin/src/main/java/com/saesig/role/DataTablesResponseDto.java
@@ -13,11 +13,15 @@ import java.util.List;
 public class DataTablesResponseDto {
     private Long recordsTotal;
     private Long recordsFiltered;
+    private int pageNumber;
+    private int pageSize;
     List<?> data = new ArrayList<>();
 
     public DataTablesResponseDto(Page<?> pageInfo, List<?> data) {
         this.recordsTotal = pageInfo.getTotalElements();
         this.recordsFiltered = pageInfo.getTotalElements();
+        this.pageNumber = pageInfo.getNumber();
+        this.pageSize = pageInfo.getSize();
         this.data = data;
     }
 }
