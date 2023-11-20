2356aff426c128bafa925c886ce4d6130f392714
wonjjong <wonjjong.dev@gmail.com>
2023-11-20 16:46:31 +0900

    feat: FileDto id, groupId 필드 추가

--- module-core/src/main/java/com/saesig/global/file/FileDto.java
+++ module-core/src/main/java/com/saesig/global/file/FileDto.java
@@ -8,11 +8,15 @@ public class FileDto {
     private final String originName;
     private final String savedName;
     private final String path;
+    private final Long id;
+    private final Long groupId;
 
     @Builder
-    public FileDto(String originName, String savedName, String path) {
+    public FileDto(String originName, String savedName, String path, Long id, Long groupId) {
         this.originName = originName;
         this.savedName = savedName;
         this.path = path;
+        this.id = id;
+        this.groupId = groupId;
     }
 }
