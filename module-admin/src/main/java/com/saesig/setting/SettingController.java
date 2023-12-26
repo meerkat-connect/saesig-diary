2092b84b4ffef616a4a7522aa82ad430f0b00933
wonjjong <wonjjong.dev@gmail.com>
2023-12-26 21:14:58 +0900

    design: 관리자 > 설정 퍼블리싱 진행

--- module-admin/src/main/java/com/saesig/setting/SettingController.java
+++ module-admin/src/main/java/com/saesig/setting/SettingController.java
@@ -1,8 +1,12 @@
 package com.saesig.setting;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SettingController {
-
+    @GetMapping("/admin/setting")
+    public String settingView() {
+        return "setting/view";
+    }
 }
