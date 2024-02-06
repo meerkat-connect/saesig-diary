e1cfee01eec87ee38779ebe18b282591279aa8c1
wonjjong <wonjjong.dev@gmail.com>
2024-02-06 18:21:11 +0900

    feat: 대시보드 > 현황 건수 조회 추가

--- module-admin/src/main/java/com/saesig/common/DashBoardMapper.java
+++ module-admin/src/main/java/com/saesig/common/DashBoardMapper.java
@@ -8,19 +8,25 @@ import java.util.Map;
 @Mapper
 public interface DashBoardMapper {
     //새로운식구 목록 조회
-    List<Map<String,Object>> selectAdoptions();
+    List<Map<String, Object>> selectAdoptions();
 
     //일상기록 목록 조회
-    List<Map<String,Object>> selectDiarys();
+    List<Map<String, Object>> selectDiarys();
 
     //관리자 게시판 목록 조회
-    List<Map<String,Object>> selectAdminPosts();
+    List<Map<String, Object>> selectAdminPosts();
 
     //회원가입 건수
-    List<Map<String,Object>> countRegisteredMembers();
+    List<Map<String, Object>> countRegisteredMembers();
 
     // 발송현황 건수
-    List<Map<String,Object>> countMessageDeliverys();
+    List<Map<String, Object>> countMessageDeliverys();
 
+    Integer countReports();
 
+    Integer countInquiries();
+
+    Integer countAdoptions();
+
+    Integer countDiarys();
 }
