5635af138837255da3e93f775031719f6a80ca75
원종민 <won.dev@mobidoo.co.kr>
2025-06-01 12:27:11 +0900

    feat: jwt accessToken 생성 구현 및 테스트 코드 작성

---
+++ module-core/src/main/java/com/saesig/config/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,88 @@
+package com.saesig.config.auth.jwt;
+
+import com.saesig.config.auth.formLogin.CustomUserDetailsService;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.expire-seconds}")
+    private long expireSecond;
+
+    public String createToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims()
+                .add("roles", roles)
+                .build();
+
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + expireSecond);
+
+        return Jwts.builder()
+                .subject(username)
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(expireDate)
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public List<String> getRoles(String token) {
+        List<?> rawList = Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("roles", List.class);
+
+        return rawList.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claimsJws =
+                    Jwts.parser()
+                            .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                            .build()
+                            .parseSignedClaims(token);
+            return true;
+        } catch (JwtException e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+}
